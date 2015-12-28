6fd5f305ec83e99575c7b8f42c93dc47ba02de4a
lixiansheng123 <617445956@qq.com>
2015-12-28 22:59:25 +0800

    客户管理

---
+++ app/src/main/java/com/yuedong/youbutie_merchant_android/SelectCarActivity.java
@@ -0,0 +1,30 @@
+package com.yuedong.youbutie_merchant_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yuedong.youbutie_merchant_android.framework.BaseActivity;
+
+public class SelectCarActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setShowContentView(R.layout.activity_select_car);
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initEvents() {
+
+    }
+
+    @Override
+    protected void ui() {
+
+    }
+}
